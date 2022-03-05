dedd27b0331b3ec11b8d4649091bd507ef691b4f
Kareem Elsafy <73001284+kareem207@users.noreply.github.com>
2022-03-05 13:12:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const fs =require("fs")
-app.listen(3000, ()=> console.log("Hello bro") )
+app.listen((process.env.PORT || 3000, ()=> console.log("Hello bro") )
 
 
 app.get('/',function (req,res) {
